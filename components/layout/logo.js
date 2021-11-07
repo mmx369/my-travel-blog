7b36a104749a5c3fc63e821149a12c20dbd31703
mmx369 <maksim@klishevich.com>
2021-11-08 00:50:25 +0300

    first commit

---
+++ components/layout/logo.js
@@ -0,0 +1,6 @@
+import classes from "./logo.module.css";
+
+export default function Logo() {
+  // eslint-disable-next-line react/no-unescaped-entities
+  return <div className={classes.logo}>Max's Blog</div>;
+}
